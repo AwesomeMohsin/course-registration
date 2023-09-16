97d8e085016997a7d058a5e557fe23bb65887a32
The Awesome <mohsindude5@gmail.com>
2023-09-16 15:59:47 +0600

    added price and credit icon\

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,4 +1,9 @@
 import PropTypes from 'prop-types';
+import { GoBook } from 'react-icons/go';
+import { FiDollarSign } from 'react-icons/fi'
+
+
+
 const Course = ({ course, handleSubmitButton }) => {
     const { id, img, title, description, price, credit } = course;
     return (
@@ -7,20 +12,27 @@ const Course = ({ course, handleSubmitButton }) => {
             <h3 className="font-semibold text-t text-lg pb-4">{title}</h3>
             <p className="text-p text-sm text-left pb-4">{description}</p>
             <div className="flex justify-between pb-4">
-                <p>$ Price : {price}</p>
-                <p> Credit : {credit}hr</p>
+                <div className='flex gap-3'>
+                    <FiDollarSign className='text-2xl'></FiDollarSign>
+                    <p className='text-p font-medium'> Price : {price}</p>
+                </div>
                 
+                <div className='flex gap-3'>
+                    <GoBook className='text-3xl'></GoBook>
+                    <p className='text-p font-medium'> Credit : {credit}hr</p>
+                </div>
+
             </div>
             <button onClick={() => handleSubmitButton(course, credit, price)}
-            className="p-btn w-full mx-auto">Select</button>
-            
+                className="p-btn w-full mx-auto">Select</button>
+
         </div>
     );
 };
 
 Course.PropTypes = {
     course: PropTypes.object.isRequired,
-    courseCredit:PropTypes.number,
+    courseCredit: PropTypes.number,
     handleSubmitButton: PropTypes.func,
 
 }
